960658439924e41cac41bd450d32307fb33ce1c5
suaaa7 <spuaaa7@gmail.com>
2019-08-31 18:11:34 +0900

    Use cache

--- SSP/request2dsp.sh
+++ SSP/request2dsp.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-curl http://localhost:8081/v1/ad -X POST -H "Content-Type:application/json" -d @test/AdReqBodyForDSP.json -i
+curl http://localhost:8081/v1/ad/g -X POST -H "Content-Type:application/json" -d @test/AdReqBodyForDSP.json -i
